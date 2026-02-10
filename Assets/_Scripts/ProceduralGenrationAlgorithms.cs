b5fe22313e8e835c051f1057cc6135a8111dd4cc
ljhx2 <ljhx2@daum.net>
2026-02-10 14:38:21 +0900

    add simple random walk algorithms

---
+++ Assets/_Scripts/ProceduralGenrationAlgorithms.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProceduralGenrationAlgorithms
+{
+    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(startPosition);
+        var previousPosition = startPosition;
+
+        for (int i = 0; i < walkLength; i++)
+        {
+            var newPosition = previousPosition + Direction2D.GetRandomCardianlDirection();
+            path.Add(newPosition);
+            previousPosition = newPosition;
+        }
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vector2Int> cardianlDirectionList = new List<Vector2Int>
+    {
+        new Vector2Int(0, 1), //Up
+        new Vector2Int(1, 0), //Right
+        new Vector2Int(0, -1), //Down
+        new Vector2Int(-1, 0), //Left
+    };
+
+    public static Vector2Int GetRandomCardianlDirection()
+    {
+        return cardianlDirectionList[Random.Range(0, cardianlDirectionList.Count)];
+    }
+}
